353460a6a5d9ca7ae51c1ddc92693c66396ddca2
Michael Kahn <mfkahn@gmail.com>
2018-01-18 12:24:40 -0500

    Fixed up command class

--- FRC3543Robot/src/org/usfirst/frc/team3543/robot/commands/DefaultAutonomousCommand.java
+++ FRC3543Robot/src/org/usfirst/frc/team3543/robot/commands/DefaultAutonomousCommand.java
@@ -1,10 +1,11 @@
-package src.org.usfirst.frc.team3543.robot.commands;
+package org.usfirst.frc.team3543.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
 
 public class DefaultAutonomousCommand extends Command {
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
