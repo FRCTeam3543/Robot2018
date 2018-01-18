99066314a7eccd6f546f167af66f5982bb8d6328
stephs33 <sskarica33@gmail.com>
2018-01-18 09:54:10 -0500

    lol

--- FRC3543Robot/src/org/usfirst/frc/team3543/robot/commands/CircleCommand.java
+++ FRC3543Robot/src/org/usfirst/frc/team3543/robot/commands/CircleCommand.java
@@ -1,15 +1,18 @@
 package org.usfirst.frc.team3543.robot.commands;
+import org.usfirst.frc.team3543.robot.Robot;
+
+// testing accuracy of autonomous driving, robot should end up where it started 
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class Circle extends CommandGroup {
+public class CircleCommand extends CommandGroup {
 
-	public Circle(double inches, double gain) {
+	public CircleCommand(double inches, double gain) {
 		requires(Robot.driveLine);
-		 //rotate a bit
-        addSequential(new RotateByAngleCommand(Math.toRadians(0.5), gain));
-		addSequential(new DriveForwardByDistanceCommand(inches, gain));		
-		 new for North Bay - dock the gear
-		addSequential(new PlaceGearCommand());
+		addSequential(new DriveForwardByDistanceCommand(40, gain));                //40 inches = 1 meter
+		addSequential(new RotateByAngleCommand(Math.toRadians(180), gain));         
+		addSequential(new DriveForwardByDistanceCommand(40, gain));	
+		addSequential(new RotateByAngleCommand(Math.toRadians(180), gain));
 	}
 	
 	@Override
