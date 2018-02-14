e8e3d6d76ddc81156f32fe16db449791e4c0640c
Michael Corriveau <itstoxication@gmail.com>
2018-02-13 21:40:05 -0500

    WIP lift

--- FRC3543Robot/src/org/usfirst/frc/team3543/robot/commands/ArcadeDriveWithJoystick.java
+++ FRC3543Robot/src/org/usfirst/frc/team3543/robot/commands/ArcadeDriveWithJoystick.java
@@ -42,7 +42,7 @@ public class ArcadeDriveWithJoystick extends Command {
 	}
 
 	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
+	protected void execute() {		
 		this.driveLine.arcadeDrive(this.joystick);
 //		this.driveLine.doTimerDelay();
 	}
